5d7530913177e94c32d4be5ae6a788eb3c20101b
Leo <ujfox13@gmail.com>
2021-11-30 18:48:41 +0300

    game-buy

--- src/components/game-buy/game-buy.js
+++ src/components/game-buy/game-buy.js
@@ -1,24 +1,30 @@
 import React from 'react';
-import { useDispatch } from 'react-redux';
-import { setItemInCart } from '../../redux/cart/reducer';
+import { useDispatch, useSelector } from 'react-redux';
+import { setItemInCart, deleteItemFromCart} from '../../redux/cart/reducer';
 import Button from '../button';
 import './game-buy.css';
 
 const GameBuy = ({ game }) => {
     const dispatch = useDispatch();
+    const items = useSelector(state => state.cart.itemsInCart);
+    const isItemsInCart = items.some(item => item.id === game.id);
 
     const handleClick = (e) => {
         e.stopPropagation();//выяснить, нужен ли он вообще
-        dispatch(setItemInCart(game))
+        if ( isItemsInCart) {
+            dispatch(deleteItemFromCart(game))
+        } else {
+            dispatch(setItemInCart(game))
+        }
     }
 
     return (
         <div className="game-buy">
             <span className="game-buy__price">{game.price} руб.</span>
             <Button
-                type='primary'
+                type= {isItemsInCart ? 'secondary' : 'primary'}
                 onClick={handleClick}>
-                В Корзину
+                    {isItemsInCart ? 'Убрать из корзины' : 'В корзину'}
             </Button>
         </div>
     )
