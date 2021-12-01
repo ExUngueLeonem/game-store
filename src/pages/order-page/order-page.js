74550214481768d3635217c57cb12ed1f2aabdc3
Leo <ujfox13@gmail.com>
2021-12-01 23:04:12 +0300

    routing & creating order-page

---
+++ src/pages/order-page/order-page.js
@@ -0,0 +1,12 @@
+import React from "react";
+import './order-page.css';
+
+const OrderPage = () => {
+    return (
+        <div className="order-page">
+            OrderPage
+        </div>
+    )
+}
+
+export default OrderPage;
