91c46d055d9dfd92767eb7a39f4936df80f4750d
Leo <ujfox13@gmail.com>
2021-12-01 22:28:57 +0300

    game-page work

--- src/pages/game-page/game-page.js
+++ src/pages/game-page/game-page.js
@@ -1,12 +1,14 @@
 import React from "react";
 import { useSelector } from "react-redux";
+import GameBuy from "../../components/game-buy";
+import GameCover from "../../components/game-cover";
 import GameGenre from "../../components/game-genre";
 import './game-page.css';
 
 const GamePage = () => {
     const game = useSelector(state => state.game.currentGame);
 
-    if(game) return null;
+    if(!game) return null;
 
     return (
         <div className="game-page">
@@ -15,9 +17,10 @@ const GamePage = () => {
                 <div className="game-page__left">
                     <iframe
                         width="90%"
-                        heigth="400px"
-                        src={game.video.src}
+                        height="400px"
+                        src={game.video}
                         title="YouTube video player"
+                        frameborder="0"
                     ></iframe>
                 </div>
 
@@ -26,6 +29,9 @@ const GamePage = () => {
                     <p>{game.description}</p>
                     <p className="secondary-text">Популярные метки:</p>
                     {game.genres.map((genre) => <GameGenre genre={genre} key={genre}/>)}
+                    <div className="game-page__buy-game">
+                        <GameBuy game={game}/>
+                    </div>
                 </div>
             </div>
         </div>
