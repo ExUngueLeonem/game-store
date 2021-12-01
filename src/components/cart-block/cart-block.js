36adb17917e0a3c547538693af492f5003b1874b
Leo <ujfox13@gmail.com>
2021-12-01 22:58:37 +0300

    cart-block refactoring

--- src/components/cart-block/cart-block.js
+++ src/components/cart-block/cart-block.js
@@ -1,16 +1,24 @@
-import React, { useState } from 'react';
+import React, { useCallback, useState } from 'react';
 import { useSelector } from 'react-redux';
 import { BiCartAlt } from 'react-icons/bi';
 import CartMenu from '../cart-menu';
 import ItemsInCart from '../items-in-cart';
 
-import './cart-block.css';
 import { calcTotalPrice } from '../utils';
+import { useNavigate } from 'react-router';
+import './cart-block.css';
 
 const CartBlock = () => {
     const [isCartMenuVisible, setIsCartMenuVisible] = useState(false)
     const items = useSelector(state => state.cart.itemsInCart);
     const totalPrice = calcTotalPrice(items);
+    const navigate = useNavigate();
+
+    //если функция передается, как пропс, самое время заюзать useCallback
+    const handleClick = useCallback(() => { //useCallback используется, чтобы не обновлялась ссылка на handleClick
+        setIsCartMenuVisible(false);
+        navigate('/order');
+    }, [navigate]);
 
     return (
         <div className="cart-block">
@@ -19,7 +27,7 @@ const CartBlock = () => {
             { totalPrice > 0 ? (
                 <span className="cart-block__total-price">{totalPrice} руб.</span> 
             ) : null}
-            { isCartMenuVisible && <CartMenu items={items} onClick={() => null}/>}
+            { isCartMenuVisible && <CartMenu items={items} onClick={handleClick}/>}
         </div>
     )
 };
