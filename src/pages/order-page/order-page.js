edefe9f18e8da28aac048a184b18f486de06c0a1
Leo <ujfox13@gmail.com>
2021-12-02 20:26:23 +0300

    order-page

--- src/pages/order-page/order-page.js
+++ src/pages/order-page/order-page.js
@@ -1,10 +1,27 @@
 import React from "react";
+import { useSelector } from "react-redux";
+import { calcTotalPrice } from '../../components/utils';
+import OrderItem from '../../components/order-item';
 import './order-page.css';
 
 const OrderPage = () => {
+    const items = useSelector(state => state.cart.itemsInCart);
+    const game = items.game;
+    if(items.length < 1) {
+        return <h1>Ваша корзина пуста</h1>
+    }
     return (
         <div className="order-page">
-            OrderPage
+            <div className="order-page__left">
+                {items.map(item =>(<OrderItem game={game}/>) )}
+            </div>
+            <div className="order-page__right">
+                <div className="order-page__total-price">
+                    <span>
+                        {calcTotalPrice()}
+                    </span>
+                </div>
+            </div>
         </div>
     )
 }
