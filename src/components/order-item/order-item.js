4afa380f03873f02a652717dbed7af1db195aca9
Leo <ujfox13@gmail.com>
2021-12-02 20:26:39 +0300

    order-item

---
+++ src/components/order-item/order-item.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { useDispatch } from "react-redux";
+import './order-item.css';
+
+const OrderItem = () => {
+const dispatch = useDispatch();
+
+    return (
+        <div className="order-item">
+            <div className="order-item__cover">
+
+            </div>
+        </div>
+    )
+}
+
+export default OrderItem;
